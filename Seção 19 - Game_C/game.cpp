2f4dd9b445ea854f00f463c80ae3f93e6bd01c4e
Lívian Pascoal <livianpasc@gmail.com>
2025-07-28 18:21:19 -0300

    funcao_principal

--- Seção 19 - Game_C/game.cpp
+++ Seção 19 - Game_C/game.cpp
@@ -7,13 +7,81 @@ int pontos = 0;
 void jogar(){
 	Calcular calc.
 	int dificuldade;
-	cout<<"Informa o nivel de dificuldade [1,2,3 ou 4]: ";
+	cout<<"Informa o nivel de dif iculdade [1,2,3 ou 4]: ";
 	cin>>dificuldade;
 	calc.dificuldade=dificuldade;
 	
 	//gera um int randomico entre 0 e 2
 	//0=somar / 1=diminiuir / 2=multiplar,
 	calc.operacao = rand()%3;
+	if (calc.dificuldade==1){
+		//facil
+		calc,valor1=rand()%11; //0 a 10
+		calc.valo2=rand)()%11;
+	} else if (calc.dificuldade==2){
+		//medio
+		calc.valo1=rand)()%101; //0 a 100
+		calc.valo2=rand)()%101; ///0 a 100
+	}  else if (calc.dificuldade==3){
+		//dificil
+		calc.valo1=rand)()%1001;
+		calc.valo2=rand)()%1001;
+	} else if (calc.dificuldade==3){
+		//ultra dificil
+		calc.valo1=rand)()%10001;
+		calc.valo2=rand)()%10001;
+	else {
+		//ultra
+		calc.valo1=rand)()%100001;
+		calc.valo2=rand)()%100001;
+	}
+	
+	int resposta;
+	cout<<"Informe o resultado para a seguinte operação: "<<endl;
+	
+	//soma
+	if (calc.operacao==0){
+		cout<<calc.valor1+calc.valor2;
+		cin>>"resultado:">>resultado;
+		
+		if (somar(resposta, calc)){
+			pontos+=1;
+			cout<<"voce tem "<<pontos<< "ponto(s)"<<endl;
+		}
+	}//diminuir
+	else if (calc.operacao==1){
+		cout<<calc.valor1-calc.valor2;
+		cin>>"resultado:">>resultado;
+		
+		if (subtrair(resposta, calc)){
+			pontos+=1;
+			cout<<"voce tem "<<pontos<< "ponto(s)"<<endl;
+		}	
+	}//multiplicar
+	else if (calc.operacao==2){
+		cout<<calc.valor1*calc.valor2;
+		cin>>"resultado:">>resultado;
+		
+		if (multiplicar(resposta, calc)){
+			pontos+=1;
+			cout<<"voce tem "<<pontos<< "ponto(s)"<<endl;
+		}
+	} //erro
+	else{
+		cout<<"operacao invalida!"<<endl;
+	}
+	
+	//recomeçar
+	cout<<"deseja recomeça o jogo? [1- sim, 0 - nao]"<<endl;
+	int continuar;
+	cin>>continuar;
+	
+	if (continuar){
+		jogar();
+	}else{
+		cout<<"voce finalizou o jogo com "<<pontos<<"pontos"<<endl;
+		exit(0);
+	}
 	
 }
 
