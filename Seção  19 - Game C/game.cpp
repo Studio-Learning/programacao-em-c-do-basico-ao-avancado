e4f10a2a73b2b3a3b1dc379b6a444a44d93f9728
Lívian Pascoal <livianpasc@gmail.com>
2025-08-27 18:18:31 -0300

    jogo_atualizacao1

--- Seção  19 - Game C/game.cpp
+++ Seção  19 - Game C/game.cpp
@@ -1,144 +1,159 @@
-#include "game.h"
-#include <cstdlib>  // para rand() e srand()
-#include <ctime>    // para time()
 #include <iostream>
+#include <stdio.h>
+#include <cstdlib> // srand, rand
+#include <ctime>   // time
 
 using namespace std;
 
-int pontos = 0;
+int pontos = 0; // global
 
-void jogar() {
-    Calcular calc;
+typedef struct {
     int dificuldade;
+    int valor1;
+    int valor2;
+    int operacao;
+    int resultado;
+} Calcular;
 
-    cout << "Informe o nivel de dificuldade [1,2,3 ou 4]: ";
-    cin >> dificuldade;
-    calc.dificuldade = dificuldade;
-
-    // Gera operação aleatória: 0 = somar, 1 = subtrair, 2 = multiplicar
-    calc.operacao = rand() % 3;
-
-    if (calc.dificuldade == 1) {
-        calc.valor1 = rand() % 11;
-        calc.valor2 = rand() % 11;
-    } else if (calc.dificuldade == 2) {
-        calc.valor1 = rand() % 101;
-        calc.valor2 = rand() % 101;
-    } else if (calc.dificuldade == 3) {
-        calc.valor1 = rand() % 1001;
-        calc.valor2 = rand() % 1001;
-    } else if (calc.dificuldade == 4) {
-        calc.valor1 = rand() % 10001;
-        calc.valor2 = rand() % 10001;
+void mostrarInfo(Calcular calc){
+    const char* op; // ponteiro para string literal
+    
+    if (calc.operacao == 0){ 
+        op = "Somar";	
+    } else if (calc.operacao == 1){
+        op = "Subtrair";
+    } else if (calc.operacao == 2){
+        op = "Multiplicar";
     } else {
-        calc.valor1 = rand() % 100001;
-        calc.valor2 = rand() % 100001;
+        op = "Operacao Invalida";
     }
+    
+    cout << "Valor 1: " << calc.valor1 << endl;
+    cout << "Valor 2: " << calc.valor2 << endl;
+    cout << "Dificuldade: " << calc.dificuldade << endl;
+    cout << "Operacao: " << op << endl;
+}
 
+int somar (int resposta, Calcular calc){
+    int resultado = calc.valor1+calc.valor2;
+    calc.resultado = resultado;
+    int certo = 0; //0 eh erro, 1 eh acerto
+    if (resposta == calc.resultado){
+        cout<<"Resposta correta"<<endl;
+        certo =1;
+    } else{
+        cout<<"Resposta errada"<<endl;
+    }
+    cout<<calc.valor1<<"+"<<calc.valor2<<"="<<calc.resultado<<endl;
+    return certo;
+}
+
+int subtrair (int resposta, Calcular calc){
+    int resultado = calc.valor1-calc.valor2;
+    calc.resultado = resultado;
+    int certo = 0; //0 eh erro, 1 eh acerto
+    if (resposta == calc.resultado){
+        cout<<"Resposta correta"<<endl;
+        certo =1;
+    } else{
+        cout<<"Resposta errada"<<endl;
+    }
+    cout<<calc.valor1<<"-"<<calc.valor2<<"="<<calc.resultado<<endl;
+    return certo;
+}
+
+int multiplicar (int resposta, Calcular calc){
+    int resultado = calc.valor1*calc.valor2;
+    calc.resultado = resultado;
+    int certo = 0; //0 eh erro, 1 eh acerto
+    if (resposta == calc.resultado){
+        cout<<"Resposta correta"<<endl;
+        certo =1;
+    } else{
+        cout<<"Resposta errada"<<endl;
+    }
+    cout<<calc.valor1<<"*"<<calc.valor2<<"="<<calc.resultado<<endl;
+    return certo;
+}
+
+void jogar(){
+    Calcular calc;
+	
+    cout<<"Informe o nivel de dificuldade [1, 2, 3 ou 4]: ";
+    cin>>calc.dificuldade;
+	
+    // gera um inteiro randomico entre 0 e 2
+    // 0 = somar, 1 = subtrair, 2 = multiplicar
+    calc.operacao = rand()% 3;
+	
+    if (calc.dificuldade == 1){
+        // facil
+        calc.valor1 = rand()%11; // 0 a 10
+        calc.valor2 = rand()%11;
+    } else if(calc.dificuldade == 2){
+        // medio
+        calc.valor1 = rand()%101; // 0 a 100
+        calc.valor2 = rand()%101;
+    } else if(calc.dificuldade == 3){
+        // dificil
+        calc.valor1 = rand()%1001; // 0 a 1000
+        calc.valor2 = rand()%1001;
+    } else if (calc.dificuldade == 4){
+        // insano
+        calc.valor1 = rand()%10001; // 0 a 10000
+        calc.valor2 = rand()%10001;
+    } else{
+        cout<<"Dificuldade invalida"<<endl;
+    }
+	
     int resposta;
-    cout << "Informe o resultado para a seguinte operacao:" << endl;
-
-    if (calc.operacao == 0) {
-        cout << calc.valor1 << " + " << calc.valor2 << endl;
-        cout << "Resultado: ";
-        cin >> resposta;
-
-        if (somar(resposta, calc)) {
-            pontos += 1;
-            cout << "Voce tem " << pontos << " ponto(s)." << endl;
+    cout<< "Informe o resultado para a seguinte operacao:" <<endl;
+	
+    // somar
+    if (calc.operacao==0){
+        cout<<calc.valor1<<"+"<<calc.valor2<<endl;
+        cin>>resposta;
+        if (somar(resposta, calc)){
+            pontos+=1;
+            cout<<"voce tem "<<pontos<<" pontos"<<endl;
         }
-    } else if (calc.operacao == 1) {
-        cout << calc.valor1 << " - " << calc.valor2 << endl;
-        cout << "Resultado: ";
-        cin >> resposta;
-
-        if (subtrair(resposta, calc)) {
-            pontos += 1;
-            cout << "Voce tem " << pontos << " ponto(s)." << endl;
-        }
-    } else if (calc.operacao == 2) {
-        cout << calc.valor1 << " * " << calc.valor2 << endl;
-        cout << "Resultado: ";
-        cin >> resposta;
-
-        if (multiplicar(resposta, calc)) {
-            pontos += 1;
-            cout << "Voce tem " << pontos << " ponto(s)." << endl;
-        }
-    } else {
-        cout << "Operacao invalida!" << endl;
     }
-
+    // subtrair
+    else if(calc.operacao==1){
+        cout<<calc.valor1<<"-"<<calc.valor2<<endl;
+        cin>>resposta;
+        if (subtrair(resposta, calc)){
+            pontos+=1;
+            cout<<"voce tem "<<pontos<<" pontos"<<endl;
+        }	
+    }
+    // multiplicar
+    else if (calc.operacao==2){
+        cout<<calc.valor1<<"*"<<calc.valor2<<endl;
+        cin>>resposta;
+        if (multiplicar(resposta, calc)){
+            pontos+=1;
+            cout<<"voce tem "<<pontos<<" pontos"<<endl;
+        }
+    }
+    else{
+        cout<<"Operacao invalida"<<endl;
+    }
+	
+    cout<<"Deseja continuar jogando? [1- sim / 0 - nao] ";
     int continuar;
-    cout << "Deseja recomecar o jogo? [1 - sim, 0 - nao]: ";
-    cin >> continuar;
-
-    if (continuar) {
+    cin>>continuar;
+	
+    if (continuar == 1){
         jogar();
-    } else {
-        cout << "Voce finalizou o jogo com " << pontos << " ponto(s)." << endl;
+    }else {
+        cout<<"Voce encerrou com "<< pontos<<" pontos"<<endl;
         exit(0);
     }
 }
 
-void mostrarInfo(Calcular calc) {
-    string operacao;
-
-    if (calc.operacao == 0) {
-        operacao = "Somar";
-    } else if (calc.operacao == 1) {
-        operacao = "Subtrair";
-    } else if (calc.operacao == 2) {
-        operacao = "Multiplicar";
-    } else {
-        operacao = "Invalido";
-    }
-
-    cout << "Operacao: " << operacao << endl;
-    cout << "Valor 1: " << calc.valor1 << endl;
-    cout << "Valor 2: " << calc.valor2 << endl;
-    cout << "Dificuldade: " << calc.dificuldade << endl;
+int main(int argc, char** argv) {
+    srand (time(NULL));
+    jogar();
+    return 0;
 }
-
-int somar(int resposta, Calcular calc) {
-    calc.resultado = calc.valor1 + calc.valor2;
-
-    if (resposta == calc.resultado) {
-        cout << "Resposta certa!" << endl;
-        cout << calc.valor1 << " + " << calc.valor2 << " = " << calc.resultado << endl;
-        return 1;
-    } else {
-        cout << "Resposta incorreta!" << endl;
-        cout << calc.valor1 << " + " << calc.valor2 << " = " << calc.resultado << endl;
-        return 0;
-    }
-}
-
-int subtrair(int resposta, Calcular calc) {
-    calc.resultado = calc.valor1 - calc.valor2;
-
-    if (resposta == calc.resultado) {
-        cout << "Resposta certa!" << endl;
-        cout << calc.valor1 << " - " << calc.valor2 << " = " << calc.resultado << endl;
-        return 1;
-    } else {
-        cout << "Resposta incorreta!" << endl;
-        cout << calc.valor1 << " - " << calc.valor2 << " = " << calc.resultado << endl;
-        return 0;
-    }
-}
-
-int multiplicar(int resposta, Calcular calc) {
-    calc.resultado = calc.valor1 * calc.valor2;
-
-    if (resposta == calc.resultado) {
-        cout << "Resposta certa!" << endl;
-        cout << calc.valor1 << " * " << calc.valor2 << " = " << calc.resultado << endl;
-        return 1;
-    } else {
-        cout << "Resposta incorreta!" << endl;
-        cout << calc.valor1 << " * " << calc.valor2 << " = " << calc.resultado << endl;
-        return 0;
-    }
-}
-
